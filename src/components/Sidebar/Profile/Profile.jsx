046a4674e26b9541036d23beef0731a7c41288ef
VarunKumarPrajapati <varunkumarprajapati57@gmail.com>
2025-01-19 10:32:18 +0530

    Profile style refactor

--- src/components/Sidebar/Profile/Profile.jsx
+++ src/components/Sidebar/Profile/Profile.jsx
@@ -22,10 +22,10 @@ export default function Profile() {
         <ProfileLoading />
       ) : (
         <div
-          className="h-full py-4 cursor-pointer lg:py-2 lg:px-6"
+          className="h-full py-4 cursor-pointer lg:py-2 lg:px-4"
           onClick={() => setModalOpen(true)}
         >
-          <div className="flex flex-row items-center justify-center h-full md:gap-x-8 lg:gap-x-2 gap-x-2">
+          <div className="flex flex-row items-center justify-start h-full md:gap-x-8 lg:gap-x-2 gap-x-2">
             <img
               draggable="false"
               src={avatars[user?.avatar]}
